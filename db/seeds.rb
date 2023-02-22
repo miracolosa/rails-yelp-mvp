13282a9de80df31def9b6194ac9de5d7f2aa8984
Milagros Lasarte <lasartemilagros@gmail.com>
2023-02-22 14:09:51 +0100

    Finished routes and controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating 5 restaurants...'
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  restaurant.save!
+  review = Review.new(
+    rating: rand(0..5),
+    content: Faker::Restaurant.review,
+    restaurant: restaurant
+  )
+  review.save!
+end
+puts 'Finished!'
